29c6d6e0525c5a636c48f3345479247a2565dea2
Gurminder SIngh <gurmindersingh@GurmindersAir2.ht.home>
2022-10-05 12:38:23 -0400

    fc

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map}
+        initialRegion={{
+          latitude: 43.595310,
+          longitude: -79.640579,
+          latitudeDelta: 0.03358723958820065,
+          longitudeDelta: 0.03358723958820065,
+        }}
+        minZoomLevel="4"
+      >
+        <Marker coordinate={{ latitude: 43.595310, longitude: -79.640579 }}
+          pinColor={"green"} // any color
+          title={"Mississauga"}
+          description={"Ontario"} />
+      </MapView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  }
 });
